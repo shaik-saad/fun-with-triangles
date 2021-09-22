1763cbb767a1c2a6022ba5df9e2591f4fbf3a140
Shaik Saad <shaiksaadullahshareef@gmail.com>
2021-09-22 21:16:50 +0530

    added quiz script file

---
+++ scripts/quiz.js
@@ -0,0 +1,7 @@
+const quizForm = document.querySelector("#quiz-form")
+const submitQuizForm = document.querySelector("#submit-quiz-btn")
+const output = document.querySelector("output")
+
+submitQuizForm.addEventListener("click", () => {
+    console.log("Hello from the other side!")
+})
