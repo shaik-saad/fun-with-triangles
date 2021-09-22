1bd59221bc6963aa194b338347e2850e22cf51aa
Shaik Saad <shaiksaadullahshareef@gmail.com>
2021-09-22 20:09:44 +0530

    feat: added error handling

--- scripts/angles.js
+++ scripts/angles.js
@@ -1,13 +1,26 @@
 const inputs = document.querySelectorAll(".angle-inputs")
 const output = document.querySelector("#output-p")
 const isTraingle = document.querySelector("#is-triangle-btn")
+const errorMessage = document.querySelector("#error-message")
 
 function checkIsTriangle(){
+    // processing
     var sum = 0
     for( var i = 0; i < inputs.length; i ++){
+        // error handling
+        if(inputs[i].value === "" || inputs[i].value === undefined){
+            return errorMessage.innerText = "Please enter angle " + i + " value"
+        }
+        if(inputs[i].value%1 !== 0){
+            return errorMessage.innerText = "Please enter angle " + i + " in number"
+        }
+        if(inputs[i].value <= 0){
+            return errorMessage.innerText = "Invalid angles. Please enter angle " + i + " above 0 degrees"
+        }
+        // adding angles
         sum = sum + Number(inputs[i].value)
     }
-    
+    // output
     if( sum === 180){
         output.innerText = "Yay! The angles you've entered form a triangle."
     } else{
