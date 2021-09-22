290dfed8843618de032a7aad0eb352f17a950315
Shaik Saad <shaiksaadullahshareef@gmail.com>
2021-09-22 22:55:53 +0530

    feat: added error handling

--- scripts/hypotenuse.js
+++ scripts/hypotenuse.js
@@ -2,6 +2,7 @@ const baseInput = document.querySelector("#base-value")
 const heightInput = document.querySelector("#height-value")
 const calculateBtn = document.querySelector("#calculate-btn")
 const output = document.querySelector("#output")
+const errorMessage = document.querySelector("#error-message")
 
 function calculateSumOfSquares(a, b){
     const sum = a*a + b*b
@@ -10,8 +11,19 @@ function calculateSumOfSquares(a, b){
 
 function calculateHypotenuse(){
     // input
-    var baseInputValue = Number(baseInput.value)
-    var heightInputValue = Number(heightInput.value)
+    var baseInputValue = baseInput.value
+    var heightInputValue = heightInput.value
+
+    // error handling
+    if(baseInputValue === "" || baseInputValue === undefined || heightInputValue === "" || heightInputValue === undefined){
+        return errorMessage.innerText = "Please enter the values."
+    }
+    if(baseInputValue%1 !== 0 || heightInputValue%1 !== 0){
+        return errorMessage.innerText = "Please enter values in number."
+    }
+    if(baseInputValue <= 0 || heightInputValue <= 0){
+        return errorMessage.innerText = "Invalid values. Please values above 0."
+    }
 
     // processing
     const sumOfSquares = calculateSumOfSquares(baseInputValue, heightInputValue)
