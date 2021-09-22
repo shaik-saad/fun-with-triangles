6f3184e725f68a8ebf68db154ed81be715dd24c2
Shaik Saad <shaiksaadullahshareef@gmail.com>
2021-09-22 19:52:32 +0530

    feat: added angles functionality

--- scripts/angles.js
+++ scripts/angles.js
@@ -3,7 +3,16 @@ const output = document.querySelector("#output-p")
 const isTraingle = document.querySelector("#is-triangle-btn")
 
 function checkIsTriangle(){
-    console.log(inputs[0].value, inputs[1].value, inputs[2].value)
+    var sum = 0
+    for( var i = 0; i < inputs.length; i ++){
+        sum = sum + Number(inputs[i].value)
+    }
+    
+    if( sum === 180){
+        output.innerText = "Yay! The angles you've entered form a triangle."
+    } else{
+        output.innerText = "Oops! The angles you've entered does not form a triangle."
+    }
 }
 
 isTraingle.addEventListener("click", checkIsTriangle)
